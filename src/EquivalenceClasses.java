9525ac5db920f58cfbeb1c6cb8f6495b38753303
SamNusstein <nusssa0@furman.edu>
2024-02-06 15:16:30 -0500

    Outlines for EquivalenceClass and LinkedEquivalence Class

--- src/EquivalenceClasses.java
+++ src/EquivalenceClasses.java
@@ -1,4 +1,39 @@
 
+import java.util.Comparator;
+
 public class EquivalenceClasses<T> {
+
+	protected Comparator<T> _comparator;
+		
+	protected List<LinkedEquivalenceClass<T>> _classes;
+		
 	
+	public EquivalenceClasses(Comparator<T> comp) {
+		
+	}
+	
+	public boolean add(T element) {
+		return false;
+		
+	}
+	public boolean contains(T target) {
+		return false;
+		
+	}
+	public int size() {
+		return 0;
+		
+	}
+	public int numClasses() {
+		return 0;
+		
+	}
+	protected int indexOfClass(T element) {
+		return 0;
+		
+	}
+	public String toString() {
+		return null;
+		
+	}
 }
--- src/LinkedEquivalenceClass.java
+++ src/LinkedEquivalenceClass.java
@@ -1,4 +1,78 @@
 
-public class LinkedEquivalenceClass {
+import input.components.linkedlist.LinkedList.Node;
 
+public class LinkedEquivalenceClass<T> {
+
+	protected T _canonical;
+
+	protected Comparator<T> _comparator;
+
+	protected LinkedList<T> _rest;
+
+
+	public LinkedEquivalenceClass(Comparator<T> comp) {
+		_rest=new LinkedList<T>();
+	}
+
+	public T canonical() {
+		return _canonical;
+
+	}
+	public boolean isempty() {
+		return _rest.isEmpty();
+
+	}
+
+	public void clear() {
+		_rest.clear();
+
+	}
+	public void clearNonCanonical() {
+
+	}
+	public int size() {
+		return _rest.size();
+
+	}
+	public boolean add(T element) {
+		if(_rest.contains(element)) {
+			return false;
+		}
+		_rest.addToBack(element);
+		if(_rest.contains(element))return true;
+		return false;
+
+
+	}
+	public boolean contains(T target) {
+		return _rest.contains(target);
+
+	}
+	public boolean belongs(T target) {
+		return false;
+
+	}
+	public boolean remove(T target) {
+		return _rest.remove(target);
+
+	}
+	public boolean removeCanonical() {
+		return _rest.remove(_canonical);
+
+	}
+	public boolean demoteAndSetCanonical(T element) {
+		return false;
+
+	}
+	public String toString() {
+		StringBuilder s=new StringBuilder("LinkedList:");
+		Node ptr=_head;
+		while(ptr!=null) {
+			s.append(ptr.data+";");
+			ptr=ptr.next;
+		}
+		return s.toString();
+		return null;
+
+	}
 }
