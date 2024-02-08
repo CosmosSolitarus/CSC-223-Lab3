3ba4354091834c603c74e71536ea8cc4e1a1d7f6
SamNusstein <nusssa0@furman.edu>
2024-02-07 22:25:21 -0500

    Started general tests

--- test/LinkedEquivalenceClassTest.java
+++ test/LinkedEquivalenceClassTest.java
@@ -1,12 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
 
 class LinkedEquivalenceClassTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGeneral() {
+		Comparator<Integer> c = new Comparator<Integer>()
+		{public int compare(Integer x, Integer y)
+		{return x%2 == y % 2 ? 0:1;}
+		};
+		LinkedEquivalenceClass<Integer> LEC =new LinkedEquivalenceClass<Integer>(c);
+		assertTrue(LEC.isempty());
+		assertFalse(LEC.contains(3));
+			for(int i=0; i<7;i++) {
+				LEC.add(i);
+			}
+		assertTrue(LEC.demoteAndSetCanonical(1));
+		assertEquals(1,LEC.canonical());
+		}
 	}
-
-}
